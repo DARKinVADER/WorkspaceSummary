f3b1a1c3b30aa06e004dee9981e3002316d78896
DARKinVADER <gabor.czibik@gmail.com>
2020-09-26 09:29:20 +0200

    TC REST API - GetAllWorkspaces

---
+++ TCRestApiGetWorkspaces/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using Xml2CSharp;
+
+namespace TCRestApiGetWorkspaces
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = httpClient.GetAsync(txtUrl.Text).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(ArrayOfstring));
+                ArrayOfstring workspaceList = (ArrayOfstring)xmlSerializer.Deserialize(new MemoryStream(Encoding.UTF8.GetBytes(content)));
+                lstWorkspace.ItemsSource = workspaceList.String;
+            }
+            else
+            {
+                MessageBox.Show("Error", "Failed to call Tosca REST API!");
+            }
+
+        }
+    }
+}
---
+++ TCRestApiGetWorkspaces/ResponseWorkspace.cs
@@ -0,0 +1,22 @@
+﻿/* 
+ Licensed under the Apache License, Version 2.0
+
+ http://www.apache.org/licenses/LICENSE-2.0
+ */
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace Xml2CSharp
+{
+	[XmlRoot(ElementName = "ArrayOfstring", Namespace = "http://schemas.microsoft.com/2003/10/Serialization/Arrays")]
+	public class ArrayOfstring
+	{
+		[XmlElement(ElementName = "string", Namespace = "http://schemas.microsoft.com/2003/10/Serialization/Arrays")]
+		public List<string> String { get; set; }
+		[XmlAttribute(AttributeName = "xmlns")]
+		public string Xmlns { get; set; }
+		[XmlAttribute(AttributeName = "i", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string I { get; set; }
+	}
+
+}
