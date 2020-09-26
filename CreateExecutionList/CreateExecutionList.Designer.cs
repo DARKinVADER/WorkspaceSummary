339402942ce742e731e49ea48d34a1f44ba34e1d
DARKinVADER <gabor.czibik@gmail.com>
2020-09-26 08:27:21 +0200

    TCAPI-CreateExecutionList

---
+++ CreateExecutionList/CreateExecutionList.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CreateExecutionList
+{
+    partial class CreateExecutionList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTestCase = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(308, 340);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(85, 52);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(112, 17);
+            this.labelPath.TabIndex = 1;
+            this.labelPath.Text = "Workspace Path";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(216, 52);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(280, 22);
+            this.txtPath.TabIndex = 2;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(208, 108);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(280, 22);
+            this.txtUser.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "User";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(216, 189);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(280, 22);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // txtTestCase
+            // 
+            this.txtTestCase.Location = new System.Drawing.Point(208, 264);
+            this.txtTestCase.Name = "txtTestCase";
+            this.txtTestCase.Size = new System.Drawing.Size(280, 22);
+            this.txtTestCase.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "TestCase";
+            // 
+            // CreateExecutionList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTestCase);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "CreateExecutionList";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTestCase;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CreateExecutionList/CreateExecutionList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tricentis.TCAPI;
+using Tricentis.TCAPIObjects.Objects;
+
+namespace CreateExecutionList
+{
+    public partial class CreateExecutionList : Form
+    {
+        public CreateExecutionList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            TCAPI.CreateInstance(new Tricentis.TCAPIObjects.TCAPIConnectionInfo());
+
+            TCWorkspace workspace = TCAPI.Instance.OpenWorkspace(txtPath.Text, txtUser.Text, txtPassword.Text);
+            TCProject project = workspace.GetProject();
+            
+            TestCase testCase = (TestCase)project.Search($"=>SUBPARTS:TestCase[Name==\"{txtTestCase.Text}\"]").FirstOrDefault();
+            if (testCase != null)
+            {
+                TCFolder executionListFolder = (TCFolder)project.Search("=>SUBPARTS:TCFolder[PossibleContent==\"Folder;ExecutionList\"]").First();
+                if (executionListFolder.IsTaskApplicable(TCTasks.Checkout))
+                {
+                    executionListFolder.Checkout();                    
+                }
+                ExecutionList executionList = executionListFolder.CreateExecutionList();
+                executionList.Name = txtTestCase.Text;
+                executionList.CreateExecutionEntry(testCase);
+                if (executionListFolder.CheckOutState.Equals(CheckOutState.CheckedOut))
+                {
+                    workspace.CheckInAll("Execution list created from code!");
+                }
+                workspace.Save();
+                MessageBox.Show("Execution list created successfully");
+            }
+            else
+            {
+                MessageBox.Show("TestCase not found!");
+            }
+            TCAPI.Instance.CloseWorkspace();
+            TCAPI.CloseInstance();
+        }
+    }
+}
